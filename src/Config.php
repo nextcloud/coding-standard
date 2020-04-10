4f5cd012760f8293e19e602651a0ecaa265e4db9
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-04-10 16:57:18 +0200

    Fully migrate to PSR2
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/Config.php
+++ src/Config.php
@@ -19,7 +19,7 @@ class Config extends Base
 	{
 		return [
 			'@PSR1' => true,
-			//'@PSR2' => true,
+			'@PSR2' => true,
 			'align_multiline_comment' => true,
 			'array_indentation' => true,
 			'array_syntax' => [
@@ -27,6 +27,13 @@ class Config extends Base
 			],
 			'blank_line_after_namespace' => true,
 			'blank_line_after_opening_tag' => true,
+			'braces' => [
+				'position_after_anonymous_constructs' => 'same',
+				'position_after_control_structures' => 'same',
+				'position_after_functions_and_oop_constructs' => 'same',
+			],
+			'elseif' => true,
+			'encoding' => true,
 			'full_opening_tag' => true,
 			'function_declaration' => [
 				'closure_function_spacing' => 'one',
@@ -35,20 +42,20 @@ class Config extends Base
 			'line_ending' => true,
 			'lowercase_keywords' => true,
 			'method_argument_space' => [],
-			//'no_break_comment' => [
-			//	'comment_text' => 'fall through'
-			//],
 			'no_closing_tag' => true,
 			'no_spaces_after_function_name' => true,
 			'no_spaces_inside_parenthesis' => true,
 			'no_trailing_whitespace' => true,
 			'no_trailing_whitespace_in_comment' => true,
+			'no_unused_imports' => true,
 			'single_blank_line_at_eof' => true,
 			'single_class_element_per_statement' => true,
 			'single_import_per_statement' => true,
 			'single_line_after_imports' => true,
 			'switch_case_space' => true,
-			'no_unused_imports' => true,
+			'visibility_required' => [
+				'elements' => ['property', 'method', 'const']
+			],
 		];
 	}
 
